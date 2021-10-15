cedfd4a754da1b4aad411e6875ba05a72cf39b99
imbolc <me@imbolc.name>
2021-10-15 20:42:02 +0300

    A better example

--- examples/counter.rs
+++ examples/counter.rs
@@ -7,14 +7,26 @@ async fn main() {
     let app = Router::new()
         .route(
             "/",
+            // Using `Cookies` extractor to access cookies
             get(|mut cookies: Cookies| async move {
-                let visited = if let Some(cookie) = cookies.get("visited") {
+                let cookie_name = "visited";
+
+                // Getting a cookie by it's name
+                let visited = if let Some(cookie) = cookies.get(cookie_name) {
                     cookie.value().parse().ok().unwrap_or(0)
                 } else {
                     0
                 };
-                cookies.add(Cookie::new("visited", (visited + 1).to_string()));
-                format!("You've been here {} times before", visited)
+
+                if visited > 10 {
+                    // Removing the cookie
+                    cookies.remove(Cookie::new(cookie_name, ""));
+                    "counter has been reset".to_string()
+                } else {
+                    // Adding (rewriting) the cookie
+                    cookies.add(Cookie::new(cookie_name, (visited + 1).to_string()));
+                    format!("You've been here {} times before", visited)
+                }
             }),
         )
         .layer(CookieLayer);
